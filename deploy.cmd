08ac7b542cb67ffc732f54876cf9f5489143f508
Andriamarosoa <tinah.andriamarosoa@gmail.com>
2021-11-23 22:34:25 +0100

    first commit

---
+++ deploy.cmd
@@ -0,0 +1,3 @@
+mvn deploy:deploy-file -Durl=file:C:\Users\huhu\.m2\repository
+                       -DrepositoryId=some.id \
+                       -Dfile=your-artifact-1.0.jar \
---
+++ src/test/java/level/mapping/MappingApplicationTests.java
@@ -0,0 +1,16 @@
+package level.mapping;
+
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
