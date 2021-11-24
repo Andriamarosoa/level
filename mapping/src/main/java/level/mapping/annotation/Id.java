f80b78a40fc2b42d72a799db06baf6eae6e6d937
Andriamarosoa <tinah.andriamarosoa@gmail.com>
2021-11-24 02:14:15 +0100

    first commit

---
+++ mapping/src/main/java/level/mapping/annotation/Id.java
@@ -0,0 +1,13 @@
+package level.mapping.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Id {
+    
+}
